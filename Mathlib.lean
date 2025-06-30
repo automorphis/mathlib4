bf325929351c2c39af25efcd0174865dd7ed91cc
automorphis <mlanetheta@gmail.com>
2025-06-29 23:11:24 -0500

    Changes made by `lean exe mk_all`

--- Mathlib.lean
+++ Mathlib.lean
@@ -3523,6 +3523,7 @@ import Mathlib.Deprecated.Cardinal.Finite
 import Mathlib.Deprecated.Cardinal.PartENat
 import Mathlib.Deprecated.Order
 import Mathlib.Deprecated.RingHom
+import Mathlib.Dynamics.Algebra
 import Mathlib.Dynamics.BirkhoffSum.Average
 import Mathlib.Dynamics.BirkhoffSum.Basic
 import Mathlib.Dynamics.BirkhoffSum.NormedSpace
